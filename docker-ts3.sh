5bd264cbd0fac30762316e02335b83dd873733a8
Knut Ahlers <knut@ahlers.me>
2018-01-07 01:46:21 +0100

    Rewrite start-script
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- docker-ts3.sh
+++ docker-ts3.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
+set -euo pipefail
+
 VOLUME=/teamspeak3
 
-echo " ----- docker-ts3 ------"
-echo "1. Linking host mounted database"
-ln -s $VOLUME/ts3server.sqlitedb /opt/teamspeak3-server_linux_amd64/ts3server.sqlitedb 
+echo "- Linking host mounted database..."
 
-echo "2. Link the files-folder into the host-mounted volume."
-mkdir -p /teamspeak3/files
+ln -s $VOLUME/ts3server.sqlitedb /opt/teamspeak3-server_linux_amd64/ts3server.sqlitedb
+
+echo "- Link the files-folder into the host-mounted volume..."
+
+mkdir -p ${VOLUME}/files
 if ! [ -L /opt/teamspeak3-server_linux_amd64/files ]; then
   rm -rf /opt/teamspeak3-server_linux_amd64/files
-  ln -s /teamspeak3/files /opt/teamspeak3-server_linux_amd64/files
+  ln -s ${VOLUME}/files /opt/teamspeak3-server_linux_amd64/files
 fi
 
-echo "3. Starting TS3-Server."
-echo "Check if ts3server.ini exists in host-mounted volume."
-if [ -f $VOLUME/ts3server.ini ]; then
-    echo "$VOLUME/ts3server.ini found. Using ini as config file."
-	echo "HINT: If this ini was transfered from another ts3-install you may want to make sure the following settings are active for the usage of host-mounted volume: (OPTIONAL)"
-	echo "query_ip_whitelist='/teamspeak3/query_ip_whitelist.txt'"
-	echo "query_ip_backlist='/teamspeak3/query_ip_blacklist.txt'"
-	echo "logpath='/teamspeak3/logs/'"
-	echo "licensepath='/teamspeak3/'" 
-	echo "inifile='/teamspeak3/ts3server.ini'"
-	/opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh \
-		inifile="/teamspeak3/ts3server.ini"
+echo "- Starting TS3-Server..."
+if [ -f "${VOLUME}/ts3server.ini" ]; then
+  echo "  '${VOLUME}/ts3server.ini' found. Using as config file."
+  echo "  HINT: If this ini was transfered from another ts3-install you may want"
+  echo "  to make sure the following settings are active for the usage of host-mounted volume:"
+  echo "  - query_ip_whitelist='${VOLUME}/query_ip_whitelist.txt'"
+  echo "  - query_ip_backlist='${VOLUME}/query_ip_blacklist.txt'"
+  echo "  - logpath='${VOLUME}/logs/'"
+  echo "  - licensepath='${VOLUME}/'"
+  echo "  - inifile='${VOLUME}/ts3server.ini'"
+  /opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh \
+    inifile="${VOLUME}/ts3server.ini"
 else
-	echo "$VOLUME/ts3server.ini not found. Creating new config file."
-	/opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh \
-		query_ip_whitelist="/teamspeak3/query_ip_whitelist.txt" \
-		query_ip_backlist="/teamspeak3/query_ip_blacklist.txt" \
-		logpath="/teamspeak3/logs/" \
-		licensepath="/teamspeak3/" \
-		inifile="/teamspeak3/ts3server.ini" \
-		createinifile=1 
+  echo "  '${VOLUME}/ts3server.ini' not found. Creating new config file."
+  /opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh \
+    createinifile=1 \
+    inifile="${VOLUME}/ts3server.ini" \
+    licensepath="${VOLUME}/" \
+    logpath="${VOLUME}/logs/" \
+    query_ip_backlist="${VOLUME}/query_ip_blacklist.txt" \
+    query_ip_whitelist="${VOLUME}/query_ip_whitelist.txt"
 fi
-
-
